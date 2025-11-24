4e5c326791c6efb9e8375b5687ec061ac26c4115
Ishan Lakhwani <ishan7023254133@gmail.com>
2025-11-24 16:45:56 +0530

    feat: calculator program complete with tests (mocha)

--- anchor/calculator/programs/calculator/src/lib.rs
+++ anchor/calculator/programs/calculator/src/lib.rs
@@ -3,14 +3,72 @@ use anchor_lang::prelude::*;
 declare_id!("Cc77fUiSGynWr1bCfUugP9PVYN5ysShksNAsFjRpR22E");
 
 #[program]
-pub mod calculator {
+pub mod anchor_calculator {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Initialize>, init_value: u32) -> Result<()> {
+        ctx.accounts.account.num = init_value;
+        Ok(())
+    }
+
+    pub fn double(ctx: Context<Double>) -> Result<()> {
+        ctx.accounts.account.num = ctx.accounts.account.num * 2;
+        Ok(())
+    }
+
+    pub fn halve(ctx: Context<Halve>) -> Result<()> {
+        ctx.accounts.account.num = ctx.accounts.account.num / 2;
+        Ok(())
+    }
+
+    pub fn add(ctx: Context<Add>, amount: u32) -> Result<()> {
+        ctx.accounts.account.num = ctx.accounts.account.num + amount;
+        Ok(())
+    }
+
+    pub fn sub(ctx: Context<Sub>, amount: u32) -> Result<()> {
+        ctx.accounts.account.num = ctx.accounts.account.num - amount;
         Ok(())
     }
 }
 
+#[account]
+pub struct DataShape {
+    num: u32,
+}
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = signer, space = 8 + 4)]
+    pub account: Account<'info, DataShape>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Double<'info> {
+    #[account(mut)]
+    pub account: Account<'info, DataShape>,
+    pub signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Halve<'info> {
+    #[account(mut)]
+    pub account: Account<'info, DataShape>,
+    pub signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Add<'info> {
+    #[account(mut)]
+    pub account: Account<'info, DataShape>,
+    pub signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Sub<'info> {
+    #[account(mut)]
+    pub account: Account<'info, DataShape>,
+    pub signer: Signer<'info>,
+}
--- anchor/calculator/tests/calculator.ts
+++ anchor/calculator/tests/calculator.ts
@@ -1,16 +1,49 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { Calculator } from "../target/types/calculator";
+import { AnchorCalculator } from "../target/types/anchor_calculator";
+import assert from "assert";
 
-describe("calculator", () => {
+describe("anchor-calculator", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.calculator as Program<Calculator>;
+  const program = anchor.workspace.anchorCalculator as Program<AnchorCalculator>;
+  const newAccount = anchor.web3.Keypair.generate();
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    // initialize with value 1
+    const tx = await program.methods.initialize(1)
+      .accounts({
+        account: newAccount.publicKey,
+        signer: anchor.getProvider().wallet.publicKey,
+      })
+      .signers([newAccount])
+      .rpc();
     console.log("Your transaction signature", tx);
   });
+
+  it("Is double!", async () => {
+    const tx = await program.methods.double()
+      .accounts({
+        account: newAccount.publicKey,
+        signer: anchor.getProvider().wallet.publicKey,
+      })
+      .rpc();
+    console.log("Your transaction signature", tx);
+    const account = await program.account.dataShape.fetch(newAccount.publicKey);
+    assert.equal(account.num, 2);
+  });
+
+  it("Is halve!", async () => {
+    const tx = await program.methods.halve()
+      .accounts({
+        account: newAccount.publicKey,
+      })
+      .rpc();
+    console.log("Your transaction signature", tx);
+    const account = await program.account.dataShape.fetch(newAccount.publicKey);
+    assert.equal(account.num, 1);
+  });
+  
+  
 });
